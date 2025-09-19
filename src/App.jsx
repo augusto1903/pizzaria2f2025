418b8e9d3efd7903b4eac0e491bdf1d76bf5267f
augusto1903 <rm95644@estudante.fieb.edu.br>
2025-09-19 07:50:22 -0300

    alterações

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,15 @@
-import { useState} from "react";
-import poke from './assets/pokeapi_256.png'
-function App() {
+import Produtos from "./pages/produtos"
 
-  // var nome = "Neymar Jr";
+const App = () => {
 
-const Formulario = (props) => {
-
-  const [nome,setNome] = useState("Memphis")
 
   return (
-    <div>
-
-      <input
-      className="nome"
-      name = "nome"
-      onChange={(e)=>{nome=e.target.value}}
-      placeholder={props.sombra != null ? props.sombra : "Texto padrão"}
-      type="text"/>
-
-      <button 
-      className="botao"
-      onClick={()=>{alert(nome)}}>
-        CLIQUE AQUI
-      </button>
-    </div>
-  )
-}
-
-  return (
-    <div>
-      <h3>Pizzaria 2F</h3>
-      <img src={poke} style={{width:200, height:100}} />
-      <img src={poke} style={{width:200, height:100}} />
-      <Formulario sombra="Digite seu nome..." />
-      <Formulario sombra="Digite seu email..." />
-      <Formulario sombra="(99)90000-0000" />
-      <Formulario/>
-      <Formulario/>
-      <Formulario/>
-
-    </div>
+    <>
+      <h1>Projeto Pizzaria</h1>
+      <Produtos></Produtos>
+      <Produtos></Produtos>
+      <Produtos></Produtos>
+    </>
   )
 }
 
