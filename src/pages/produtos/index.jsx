a06c6cdb76ff8401fd90b4e746802d18eeab28f1
augusto1903 <rm95644@estudante.fieb.edu.br>
2025-09-19 08:30:32 -0300

    alterar

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,19 +1,18 @@
+import axios from "axios"
 const Produtos = () => {
-    //Objeto com a lista de pizzas
-    const pizza = [
-        'Pizza de Muçarela',
-        'Pizza de Calabresa',
-        'Pizza Baiana', 'Pizza Quatro Queijos'
-    ]
+    //consumo de um endpoint com as pizzas
+    axios.get("http://viacep.com.br/ws/01010000/json")
+    .then(responde=>{
+        console.log(Response.data)
+    })
+
+
     // Iteraçãoi da lista de pizzas
     const ListaPizzas = pizzas.map(pizza=><li>{pizza}</li>)
     return (
         <div>
             <ul>
-                <li>Pizza De Muçarela</li>
-                <li>Pizza de Calabreza</li>
-                <li>Pizza Portuguesa</li>
-                <li>Pizza Baiana</li>
+             {ListaPizzas}
             </ul>
         </div>
     )
